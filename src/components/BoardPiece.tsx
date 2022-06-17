4b33279c2ac664228053b055ad07cf79630cbcfc
simon <stsveig@gmail.com>
2022-06-17 14:34:35 -0700

    refactor BoardPiece logic

--- src/components/BoardPiece.tsx
+++ src/components/BoardPiece.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import {
-  useCurrentPieceTurn,
-  useValidPieceMoves,
-} from "../context/OthelloContext";
-import {
-  Cell,
-  CellPosition,
-  comapreTwoCellPositions,
-} from "../othelloLogic/board";
+import { useCurrentPieceTurn } from "../context/OthelloContext";
+import { Cell, Piece } from "../othelloLogic/board";
 
 type BoardPieceProps = {
   cell: Cell;
@@ -18,24 +11,29 @@ type BoardPieceProps = {
 const BoardPiece = ({ cell }: BoardPieceProps) => {
   const currentPieceTurn = useCurrentPieceTurn();
 
-  const validMovesForCurrentTurn = useValidPieceMoves(currentPieceTurn);
-
   let pieceUI = "";
+
   const basicPieceStyle = "rounded-full w-[85%] h-[85%]";
 
   if (cell.state === "empty") {
-    if (
-      validMovesForCurrentTurn &&
-      isCellValidMove(cell, validMovesForCurrentTurn)
-    ) {
-      pieceUI = "border border-[#303030] opacity-60";
+    if (currentPieceTurn === "gameOver") {
+      // game is over Board Piece UI
+    } else {
+      // current piece is 'black' | 'white'
+      if (isCellHasValidMovesFor(currentPieceTurn, cell)) {
+        pieceUI = "border border-[#303030] opacity-60";
+      }
     }
   } else {
     pieceUI = cell.state === "black" ? "bg-black" : "bg-white";
   }
 
-  function isCellValidMove(cell: CellPosition, validMoves: CellPosition[]) {
-    return validMoves.find((move) => comapreTwoCellPositions(move, cell));
+  function isCellHasValidMovesFor(currentPieceTurn: Piece, cell: Cell) {
+    if (currentPieceTurn === "black") {
+      return cell.validBlackMoves?.length;
+    } else {
+      return cell.validWhiteMoves?.length;
+    }
   }
 
   return (
